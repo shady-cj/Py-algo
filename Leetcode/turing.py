147fdb83ec01a342dd9be396b53b6ab64f1f5ee0
ERINFOLAMI PETER <66220414+shady-cj@users.noreply.github.com>
2022-08-18 22:13:25 +0100

    Update turing.py

--- Leetcode/turing.py
+++ Leetcode/turing.py
@@ -27,27 +27,3 @@ num = baseball(["5", "2", "C", "D", "+"])
 print(num)
 num = baseball(["5", "-2", "4", "C", "D", "9", "+", "+"])
 print(num)
-
-
-# More Optimized Solution
-
-def Optimized_solution(ops, index = 0):
-    sum = 0
-    if (index == len(ops)):
-        return 0
-    present = int(ops[index])
-    prev = int(ops[index - 1])
-    if (ops[index] == "+"):
-        sum += present + prev
-    elif (ops[index] == "D"):
-        sum += prev * 2
-    elif (ops[index] == "C"):
-        sum -= prev
-    else:
-        sum += present
-    sum += Optimized_solution(ops, index + 1)
-    
-    return sum
-    
-print("Optimized--------------------")
-print(Optimized_solution(["5", "2", "C", "D", "+"]))
